34b91197303f7c4019fc6fe933c57732e9526b8a
KarataBG <niki.karata@gmail.com>
2025-06-06 22:29:36 +0300

    Added Index page and changed Login page

---
+++ src/main/java/KarataBG/SpendingTracker/Index.java
@@ -0,0 +1,12 @@
+package KarataBG.SpendingTracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/java/KarataBG/SpendingTracker/SecurityConfig.java
@@ -0,0 +1,32 @@
+package KarataBG.SpendingTracker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login" ,"/index.html", "/css/**", "/js/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
